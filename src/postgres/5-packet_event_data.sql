1cdc17aeee293b977e3ac05699fa6cb779cf3d52
Ramunas <ralekn@gmail.com>
2020-09-16 23:58:13 +0100

    packet event data sproc params

--- src/postgres/5-packet_event_data.sql
+++ src/postgres/5-packet_event_data.sql
@@ -83,65 +83,65 @@ CREATE TABLE IF NOT EXISTS SpeedTrap (
       REFERENCES PacketEventData (ID)
 );
 
-CREATE OR REPLACE PROCEDURE insert_packet_event_data("ID" uuid, "PacketHeader_ID" uuid)
+CREATE OR REPLACE PROCEDURE insert_packet_event_data("ID" uuid, "PacketHeader_ID" uuid, "EventStringCode" text)
 LANGUAGE 'plpgsql'
 AS $BODY$
 	BEGIN
 		INSERT INTO PacketEventData 
-		VALUES ("ID", "PacketHeader_ID");
+		VALUES ("ID", "PacketHeader_ID", "EventStringCode");
 	END;
 $BODY$;
 
-CREATE OR REPLACE PROCEDURE insert_fastest_lap("ID" uuid, "PacketEventData_ID" uuid)
+CREATE OR REPLACE PROCEDURE insert_fastest_lap("ID" uuid, "PacketEventData_ID" uuid, "VehicleIdx" integer, "LapTime" double precision)
 LANGUAGE 'plpgsql'
 AS $BODY$
 	BEGIN
 		INSERT INTO FastestLap 
-		VALUES ("ID", "PacketEventData_ID");
+		VALUES ("ID", "PacketEventData_ID", "VehicleIdx", "LapTime");
 	END;
 $BODY$;
 
-CREATE OR REPLACE PROCEDURE insert_retirement("ID" uuid, "PacketEventData_ID" uuid)
+CREATE OR REPLACE PROCEDURE insert_retirement("ID" uuid, "PacketEventData_ID" uuid, "VehicleIdx" integer)
 LANGUAGE 'plpgsql'
 AS $BODY$
 	BEGIN
 		INSERT INTO Retirement 
-		VALUES ("ID", "PacketEventData_ID");
+		VALUES ("ID", "PacketEventData_ID", "VehicleIdx");
 	END;
 $BODY$;
 
-CREATE OR REPLACE PROCEDURE insert_teammate_in_pits("ID" uuid, "PacketEventData_ID" uuid)
+CREATE OR REPLACE PROCEDURE insert_teammate_in_pits("ID" uuid, "PacketEventData_ID" uuid, "VehicleIdx" integer)
 LANGUAGE 'plpgsql'
 AS $BODY$
 	BEGIN
 		INSERT INTO TeamMateInPits 
-		VALUES ("ID", "PacketEventData_ID");
+		VALUES ("ID", "PacketEventData_ID", "VehicleIdx");
 	END;
 $BODY$;
 
-CREATE OR REPLACE PROCEDURE insert_race_winner("ID" uuid, "PacketEventData_ID" uuid)
+CREATE OR REPLACE PROCEDURE insert_race_winner("ID" uuid, "PacketEventData_ID" uuid, "VehicleIdx" integer)
 LANGUAGE 'plpgsql'
 AS $BODY$
 	BEGIN
 		INSERT INTO RaceWinner 
-		VALUES ("ID", "PacketEventData_ID");
+		VALUES ("ID", "PacketEventData_ID", "VehicleIdx");
 	END;
 $BODY$;
 
-CREATE OR REPLACE PROCEDURE insert_penalty("ID" uuid, "PacketEventData_ID" uuid)
+CREATE OR REPLACE PROCEDURE insert_penalty("ID" uuid, "PacketEventData_ID" uuid, "PenaltyType" integer, "InfringementType" integer, "VehicleIdx" integer, "Time" integer, "LapNum" integer, "PlacesGained" integer)
 LANGUAGE 'plpgsql'
 AS $BODY$
 	BEGIN
 		INSERT INTO Penalty 
-		VALUES ("ID", "PacketEventData_ID");
+		VALUES ("ID", "PacketEventData_ID", "PenaltyType", "InfringementType", "VehicleIdx", "Time", "LapNum", "PlacesGained");
 	END;
 $BODY$;
 
-CREATE OR REPLACE PROCEDURE insert_speed_trap("ID" uuid, "PacketEventData_ID" uuid)
+CREATE OR REPLACE PROCEDURE insert_speed_trap("ID" uuid, "PacketEventData_ID" uuid, "VehicleIdx" integer, "Speed" double precision)
 LANGUAGE 'plpgsql'
 AS $BODY$
 	BEGIN
 		INSERT INTO SpeedTrap 
-		VALUES ("ID", "PacketEventData_ID");
+		VALUES ("ID", "PacketEventData_ID", "VehicleIdx", "Speed");
 	END;
 $BODY$;
